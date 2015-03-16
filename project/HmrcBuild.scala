1ffe0e471d400442ccc459155b233ee2c8cf6569
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-03-16 17:18:15 +0000

    Remove references to sonatype

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -50,7 +50,7 @@ object HmrcBuild extends Build {
       crossScalaVersions := Seq("2.11.6")
     )
     .settings(SbtBuildInfo(): _*)
-    .settings(SonatypeBuild(): _*)
+    .settings(POMMetadata())
     .settings(Headers(): _ *)
 }
 
@@ -61,22 +61,22 @@ object Headers {
           "scala" ->(
               HeaderPattern.cStyleBlockComment,
               """|/*
-         | * Copyright 2015 HM Revenue & Customs
-         | *
-         | * Licensed under the Apache License, Version 2.0 (the "License");
-         | * you may not use this file except in compliance with the License.
-         | * You may obtain a copy of the License at
-         | *
-         | *   http://www.apache.org/licenses/LICENSE-2.0
-         | *
-         | * Unless required by applicable law or agreed to in writing, software
-         | * distributed under the License is distributed on an "AS IS" BASIS,
-         | * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-         | * See the License for the specific language governing permissions and
-         | * limitations under the License.
-         | */
-         |
-         |""".stripMargin
+                 | * Copyright 2015 HM Revenue & Customs
+                 | *
+                 | * Licensed under the Apache License, Version 2.0 (the "License");
+                 | * you may not use this file except in compliance with the License.
+                 | * You may obtain a copy of the License at
+                 | *
+                 | *   http://www.apache.org/licenses/LICENSE-2.0
+                 | *
+                 | * Unless required by applicable law or agreed to in writing, software
+                 | * distributed under the License is distributed on an "AS IS" BASIS,
+                 | * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                 | * See the License for the specific language governing permissions and
+                 | * limitations under the License.
+                 | */
+                 |
+                 |""".stripMargin
                 )
           ),
       (compile in Compile) <<= (compile in Compile) dependsOn (createHeaders in Compile),
@@ -84,12 +84,8 @@ object Headers {
     )
  }
 
-object SonatypeBuild {
-
-  import xerial.sbt.Sonatype._
-
+object POMMetadata {
   def apply() = {
-    sonatypeSettings ++ Seq(
       pomExtra :=
         <url>https://www.gov.uk/government/organisations/hm-revenue-customs</url>
           <licenses>
@@ -110,6 +106,5 @@ object SonatypeBuild {
               <url>http://www.equalexperts.com/</url>
             </developer>
           </developers>
-    )
   }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -16,8 +16,6 @@ resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-utils" % "2.2.0")
 
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.2")
-
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.8")
 
 addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.1.1")
